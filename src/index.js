c4eda4f784311b3679784f98b5910c927ad0e83e
Gabriel Soares <gabrielso4res@gmail.com>
2021-04-24 10:28:51 -0300

    adicionado o saque

--- src/index.js
+++ src/index.js
@@ -23,6 +23,17 @@ function verifyIfExistsAccountCPF(request, response, next){
     return next();
 }
 
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else{
+            return acc - operation.amount;
+        }
+    }, 0);
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name} = request.body;
 
@@ -60,6 +71,26 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) =>{
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
-})
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) =>{
+    const { customer } = request;
+    const { amount } = request.body;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({ error: "Insuficient founds!" })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
