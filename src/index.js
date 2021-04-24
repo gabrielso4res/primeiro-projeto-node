2c35b642298f6c9aa2579018b9dc035c0f09fc9b
Gabriel Soares <gabrielso4res@gmail.com>
2021-04-24 14:11:32 -0300

    projeto atualizado com busca, update, delete e balance(saldo)

--- src/index.js
+++ src/index.js
@@ -40,6 +40,10 @@ app.post("/account", (request, response) => {
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf);
 
+    if(customerAlreadyExists){
+        return response.status(400).json({ error: "Customer Already Exists!" })
+    }
+
     customers.push({
         cpf,
         name,
@@ -91,6 +95,51 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) =>{
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF,(request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    // 10/10/21
+    const statement = customer.statement.filter((statement) =>
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF,(request, response) =>{
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    //splice
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
 });
 
 app.listen(3333);
